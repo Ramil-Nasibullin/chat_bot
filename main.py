a24e7fd02a4ec6723e1d0e960c793bc95b19ec6d
Ramil-Nasibullin <ramil-hamza@yandex.ru>
2020-03-28 19:56:52 +0300

    Hide token

---
+++ main.py
@@ -0,0 +1,23 @@
+import apiai
+
+
+import json
+
+
+def send_massage(message):
+    request = apiai.ApiAI('ff096334f51942f89f511677ce6c66d1').text_request()
+    request.lang = 'ru'
+    request.session_id = 'session 1'
+    request.query = message
+    response = json.loads(request.getresponse().read())
+    print(response['result']['fulfillment']['speech'])
+    return response['result']['action']
+
+print('Напишите сообщение или напишите выход')
+message = input()
+action = None
+while True:
+    action = send_massage(message)
+    if action == 'asmalltalk.greetings.bye':
+        break
+    message = input()
